cb977f1bd24d5dd6b27737000944f238bd6de3d6
jonwong203 <jonwong@sas.upenn.edu>
2022-11-05 22:54:58 -0700

    mumbai testing ban spencer with governance

--- scripts/deploy_DAOPoint.ts
+++ scripts/deploy_DAOPoint.ts
@@ -1,20 +1,21 @@
 // const { ethers } = require("hardhat");
 
-async function main() {
+async function deployDAO() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
-    const governorAddr = "";
+    const governorAddr = "0x1BE1601d71c382A25C306C1feb427fe79AEFA18c";
+    const serverID = "1038181586232950945";
 
     const DAOPoint = await ethers.getContractFactory("DAOPoint");
-    const daoPoint = await DAOPoint.deploy(governorAddr);
+    const daoPoint = await DAOPoint.deploy(governorAddr, serverID);
     await daoPoint.deployed();
-    console.log("Boiler address:", daoPoint.address);
+    console.log("DAOpoint address:", daoPoint.address);
     console.log("Transaction hash:", daoPoint.deployTransaction.hash);
   }
   
-  main()
+  deployDAO()
     .then(() => process.exit(0))
     .catch((error) => {
       console.error(error);
