ef0841d1260684a7dcd5c171503bf7d347901935
calebcapoccia <calebcapoccia@college.harvard.edu>
2022-11-05 23:35:22 -0700

    Modified to create roles with midpoint

--- contracts/DAOPoint.sol
+++ contracts/DAOPoint.sol
@@ -18,6 +18,7 @@ contract DAOPoint is Ownable{
     // These events are for demonstration purposes only; they can be removed without effect.
     event banRequestMade(uint256 requestId, string SERVER_ID, string USER_ID);
     event unBanRequestMade(uint256 requestId, string SERVER_ID, string USER_ID);
+    event createRoleRequestMade(uint256 requestId, string SERVER_ID, string ROLE_NAME, string ROLE_COLOR);
     event addRoleRequestMade(uint256 requestId, string SERVER_ID, string USER_ID, string ROLE_ID);
     event removeRoleRequestMade(uint256 requestId, string SERVER_ID, string USER_ID, string ROLE_ID);
     event ResponseReceived(uint256 requestId);
@@ -81,7 +82,7 @@ contract DAOPoint is Ownable{
         uint256 requestId = IMidpoint(startpointAddress).callMidpoint(createRoleMidpointID, args);
 
         // For Demonstration Purposes Only
-        emit RequestMade(requestId, SERVER_ID, USER_ID);
+        emit createRoleRequestMade(requestId, SERVER_ID, ROLE_NAME, ROLE_COLOR;
     }
 
     function AddRole(string memory USER_ID, string memory ROLE_ID) public onlyOwner {
